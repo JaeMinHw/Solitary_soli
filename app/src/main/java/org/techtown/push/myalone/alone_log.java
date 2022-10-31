a25d1cb4f3fb81284082249e8b5aa6c40a05b824
JM <bosik99@naver.com>
2022-10-31 15:11:32 +0900

    solitary commit

---
+++ app/src/main/java/org/techtown/push/myalone/alone_log.java
@@ -0,0 +1,14 @@
+package org.techtown.push.myalone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class alone_log extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alone_log);
+    }
+}
---
+++ app/src/main/java/org/techtown/push/myalone/call_emergency.java
@@ -0,0 +1,14 @@
+package org.techtown.push.myalone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class call_emergency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_emergency);
+    }
+}
